c49aa177537a37cb45cf828884fd1fcb9b42079e
Robby Zambito <Zambito101@gmail.com>
2018-09-13 13:57:13 -0400

    Initial commit

---
+++ server.go
@@ -0,0 +1,24 @@
+/* Barebones web server which serves up a static HTML file
+ * Based on the example given here:
+ * https://www.alexedwards.net/blog/serving-static-sites-with-go
+ *
+ * This can be the base for a file upload service. 
+ */
+
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("static"))
+	http.Handle("/", fileServer)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
+
+}
+
+
